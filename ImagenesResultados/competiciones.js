d67ef205cf44c118e0fbec95ad7e62d34669c5ec
ismafelgon <ismafelgon@gmail.com>
2026-03-10 18:06:47 -0300

    Add files via upload

---
+++ ImagenesResultados/competiciones.js
@@ -0,0 +1,3 @@
+var DATOS_COMPETIDORES = {
+    "liga profesional": { color: "#0C004A", logo: "https://i.imgur.com/U9STm60.png" },
+};
---
+++ ImagenesResultados/jugadores.js
@@ -0,0 +1,5 @@
+// Archivo: equipos.js
+var DATOS_EQUIPOS = {
+    "david romero": { color: "#024084", escudo: "https://i.imgur.com/UeakNx4.png" },
+    "angel di maria": { color: "#F3D83E", escudo: "https://i.imgur.com/QVmzWIU.png" },
+};
